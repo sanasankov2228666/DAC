ac9bf5e4ce8746270f8634fabbb61c6b1dd90510
Alexander Kuptsov <sanasankov087@gmail.com>
2026-03-05 15:59:55 +0300

    доделал

--- signal_generator.py
+++ signal_generator.py
@@ -1,20 +1,8 @@
 import time
+import numpy
 
 def get_sin_wave_amplitude(freq, time):
-    from math import sin, pi
-    raw = sin(2.0 * pi * freq * time) + 1
-    
-    return raw / 2.0
+    return (numpy.sin(2 * numpy.pi * freq * time) + 1) / 2
 
 def wait_for_sampling_period(sampling_frequency):
-    period = 1.0 / sampling_frequency
-    time.sleep(period)
-
-def get_triangle_wave_amplitude(freq, time):
-    period = 1.0 / freq
-    time %= period
-    if time < period / 2:
-        return time / (period / 2)
-    else:
-        time %= period / 2
-        return 1 - time / (period / 2)
+    time.sleep(1 / sampling_frequency)
