a80a6839d07e9997a5b9bfd743644d8c75beb98f
Alexander Kuptsov <sanasankov087@gmail.com>
2026-03-05 16:30:09 +0300

    доделал

--- pwm_dac.py
+++ pwm_dac.py
@@ -1,29 +1,31 @@
 import RPi.GPIO as GPIO
 
-def dec2bin(value):
-    return [int(bit) for bit in bin(value)[2:].zfill(8)] 
-
 class PWM_DAC:
-    def __init__(self, gpio_bits, pwm_frequency, dynamic_range, verbose=False):
-        self.gpio_bits = gpio_bits
-        self.pwm_frequency = pwm_frequency
+    def __init__(self, gpio_pin, pwm_frequency, dynamic_range, verbose = False):
+        self.gpio_pin = gpio_pin
+        self.freq = pwm_frequency
         self.dynamic_range = dynamic_range
         self.verbose = verbose
 
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.gpio_bits, GPIO.OUT, initial=0)
-
-        self.pwm = GPIO.PWM(gpio_bits, pwm_frequency)
+        GPIO.setup(self.gpio_pin, GPIO.OUT, initial = 0)
+        self.pwm = GPIO.PWM(self.gpio_pin, self.freq)
         self.pwm.start(0)
 
     def deinit(self):
-        GPIO.output(self.gpio_bits, 0)
+        self.pwm.stop()
+        GPIO.output(self.gpio_pin, 0)
         GPIO.cleanup()
 
     def set_voltage(self, voltage):
-        duty_cycle = (voltage / self.dynamic_range) * 100
+        if  (0.0 <= voltage <= self.dynamic_range):
+            duty_cycle = (voltage / self.dynamic_range * 100)
+        else:
+            print(f"Напряжение выходит за динамический диапазон ЦАП (0.00 - {self.dynamic_range:.2f} В)")
+            print("Устанавливаем 0.0 В")
+            duty_cycle = 0
         self.pwm.ChangeDutyCycle(duty_cycle)
-
+        print(f"Коэффициент заполнения {duty_cycle:.2f} B")
 
 if __name__ == "__main__":
     try:
@@ -31,10 +33,9 @@ if __name__ == "__main__":
 
         while True:
             try:
-                voltage = float(input("Введите напряжение в Вольтах: "))
+                voltage = float(input("Введите напряжение в вольтах: "))
                 dac.set_voltage(voltage)
-
             except ValueError:
-                print("Вы ввели не число. Попробуйте ещё раз\n")
+                print("Вывведи не число. Попробуйте еще раз\n")
     finally:
         dac.deinit()
